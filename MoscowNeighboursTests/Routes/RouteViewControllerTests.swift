4af54e49fd9847a969c82456b0974a8011a4c1dc
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-07 23:50:51 +0300

    `RouteViewController` does not fetch routes on init

--- MoscowNeighboursTests/Routes/RouteViewControllerTests.swift
+++ MoscowNeighboursTests/Routes/RouteViewControllerTests.swift
@@ -10,4 +10,34 @@ import XCTest
 
 class RouteViewControllerTests: XCTestCase {
     
+    func test_init_doesNotLoadRoutes() {
+        let (sut, loader) = makeSUT()
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: RouteViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let builder = Builder()
+        let storage = RoutesStorage(
+            routesService: loader,
+            purchaseService: builder.purchaseService,
+            userState: builder.userState,
+            routesDescriptionBuilder: builder)
+        let sut = builder.buildRouteViewController(with: storage)
+        return (sut, loader)
+    }
+    
+    private final class LoaderSpy: RoutesProvider {
+        var observers: [String : RouteServiceDelegate] = [:]
+        var loadCallCount: Int = 0
+        
+        func fetchRoutes() {
+            loadCallCount += 1
+        }
+    }
+    
+    
 }
