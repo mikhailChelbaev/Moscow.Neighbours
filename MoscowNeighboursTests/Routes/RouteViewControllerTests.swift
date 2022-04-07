8b2ec84abbd97e949f1e488e0cba4e8d009ad39c
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-07 23:42:42 +0300

    create test folder and init `RouteViewControllerTests`

---
+++ MoscowNeighboursTests/Routes/RouteViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  RouteViewControllerTests.swift
+//  MoscowNeighboursTests
+//
+//  Created by Mikhail on 07.04.2022.
+//
+
+import XCTest
+@testable import MoscowNeighbours
+
+class RouteViewControllerTests: XCTestCase {
+    
+}
