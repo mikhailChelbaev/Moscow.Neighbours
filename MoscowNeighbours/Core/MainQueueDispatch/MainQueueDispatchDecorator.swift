58de90bc5c30bc0fbfd8c1cf029e74b46695d930
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-09 19:36:26 +0300

    add `MainQueueDispatchDecorator`

---
+++ MoscowNeighbours/Core/MainQueueDispatch/MainQueueDispatchDecorator.swift
@@ -0,0 +1,24 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  MoscowNeighbours
+//
+//  Created by Mikhail on 09.04.2022.
+//
+
+import Foundation
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        
+        completion()
+    }
+}
