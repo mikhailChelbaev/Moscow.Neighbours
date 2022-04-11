27acf6a4c63201dbc405e8c2b38b51e1a3de21b4
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-12 00:59:25 +0300

    create `RouteDescriptionViewControllerTests`

---
+++ MoscowNeighboursTests/RouteDescription/RouteDescriptionViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  RouteDescriptionViewControllerTests.swift
+//  MoscowNeighboursTests
+//
+//  Created by Mikhail on 12.04.2022.
+//
+
+import XCTest
+
+class RouteDescriptionViewControllerTests: XCTestCase {
+    
+}
+
