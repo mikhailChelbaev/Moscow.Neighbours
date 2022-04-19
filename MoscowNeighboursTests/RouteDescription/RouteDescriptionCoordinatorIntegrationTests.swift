74fcd352f75f87bddbf184e1239f13bd163ec6e6
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-19 21:37:50 +0300

    create `RouteDescriptionCoordinatorIntegrationTests`

---
+++ MoscowNeighboursTests/RouteDescription/RouteDescriptionCoordinatorIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  RouteDescriptionCoordinatorIntegrationTests.swift
+//  MoscowNeighboursTests
+//
+//  Created by Mikhail on 19.04.2022.
+//
+
+import XCTest
+
+class RouteDescriptionCoordinatorIntegrationTests: XCTestCase {
+    
+}
