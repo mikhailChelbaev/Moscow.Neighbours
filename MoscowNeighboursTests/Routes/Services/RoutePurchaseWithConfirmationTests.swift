176cdfddcf2d9218f9993c96e1c0bfea2f47c26e
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-23 22:48:44 +0300

    create `RoutePurchaseWithConfirmationTests` and reorganize test files

---
+++ MoscowNeighboursTests/Routes/Services/RoutePurchaseWithConfirmationTests.swift
@@ -0,0 +1,14 @@
+//
+//  RoutePurchaseWithConfirmationTests.swift
+//  MoscowNeighboursTests
+//
+//  Created by Mikhail on 23.04.2022.
+//
+
+import XCTest
+
+class RoutePurchaseWithConfirmationTests: XCTestCase {
+    
+    
+    
+}
