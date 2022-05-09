d8b136046cd074cba63d6fd161faeaff27851f48
Mikhail <chelbaev.mihail70@gmail.com>
2022-05-09 21:50:11 +0300

    extract `Achievement` to its own file

---
+++ MoscowNeighbours/UI/Achievements/Services/Achievement.swift
@@ -0,0 +1,14 @@
+//
+//  Achievement.swift
+//  MoscowNeighbours
+//
+//  Created by Mikhail on 09.05.2022.
+//
+
+import Foundation
+
+public struct Achievement {
+    public let name: String
+    public let imageURL: String
+    public let date: Date?
+}
--- MoscowNeighbours/UI/Achievements/Services/AchievementsProvider.swift
+++ MoscowNeighbours/UI/Achievements/Services/AchievementsProvider.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-public struct Achievement {
-    public let name: String
-    public let imageURL: String
-    public let date: Date?
-}
-
 public protocol AchievementsProvider {
     typealias Result = Swift.Result<[Achievement], Error>
     
